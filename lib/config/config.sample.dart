1468e72acfddca431d40e631c46f3c0b2e9de7c5
Dmytro Portenko <troublesohard@gmail.com>
2019-01-14 15:04:30 +0200

    magento config file

---
+++ lib/config/config.sample.dart
@@ -0,0 +1,5 @@
+class Config {
+  final String baseUrl = 'http://magento.baseurl/';
+  final String accessToken = 'admin_access_token';
+  final String homeCmsBlockId = 'cms_block_with_mobile_homescreen_config';
+}
