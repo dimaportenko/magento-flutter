709ef42c1e09595d0db6d229c4d75ba24f051307
Dmytro Portenko <troublesohard@gmail.com>
2019-01-15 00:46:10 +0200

    home screen swiper

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_page_indicator/flutter_page_indicator.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import '../blocs/provider.dart';
 import '../widgets/refresh.dart';
+import '../config/config.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -26,12 +29,47 @@ class Home extends StatelessWidget {
           );
         }
 
-        print(snapshot.data);
+        final slider = snapshot.data['slider'];
+        final images = slider.map((slide) => slide['image']);
+        final titles = slider.map((slide) => slide['title']);
+        print(images);
 
         return Refresh(
           child: ListView(
             children: <Widget>[
-              Text('done'),
+              Container(
+                height: 200,
+                child: Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Stack(
+                      children: <Widget>[
+                        Container(
+                            height: 200,
+                            child: Image.network(
+                              '${config.baseUrl}media/${images.elementAt(index)}',
+                              fit: BoxFit.fitHeight,
+                            )),
+                        Center(
+                          child: Container(
+                            padding: EdgeInsets.only(left: 30, right: 30),
+                            child: Text(
+                              titles.elementAt(index),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  itemCount: images.length,
+                  pagination: SwiperPagination(),
+                ),
+              ),
             ],
           ),
         );
